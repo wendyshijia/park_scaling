54e6d25b0d80376947cc014f7748a8ac750b67f6
WendySJW <wendy.shijia@gmail.com>
2024-09-06 10:50:41 +0800

    add button

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+// import gsap from "gsap";
 console.clear();
+// gsap.registerPlugin(ScrollTriger, ScrollToPlugin);
 
 const canvas = document.getElementById("hero-lightpass");
 const context = canvas.getContext("2d");
+const button = document.getElementById("scroll-button");
 
 canvas.width = 1800;
 canvas.height = 900;
@@ -39,3 +42,34 @@ function render() {
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.drawImage(images[sequencer.frame], 0, 0); 
 }
+
+// function scrollToPosition(y) {
+//   gsap.to(window, {
+//     duration: 1,
+//     scrollTo: { y: y, autoKill: false },
+//     ease: "power2.inOut"
+//   });
+// }
+
+button.addEventListener('click', function() {
+  console.log('clicked')
+  gsap.to(window, {
+    duration: 1,
+    scrollTo: { y: 325, autoKill: false },
+    ease: "power2.inOut"
+  });
+});
+
+ScrollTrigger.create({
+  start: 0,
+  end: "max",
+  onUpdate: (self) => {
+    const scrollY = Math.round(self.scroll()); // Round to nearest integer
+    
+    if (scrollY > 323 && scrollY < 327) {
+      button.classList.add('at-target-scroll');
+    } else {
+      button.classList.remove('at-target-scroll');
+    }
+  }
+});
